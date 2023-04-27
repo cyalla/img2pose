435126edf72ec539c1601cbab950c1b01ac00e49
chandra <chandra.sekhar.yalla@oracle.com>
2023-04-27 07:32:28 +0530

    backbone modified  to backbone1

--- img2pose.py
+++ img2pose.py
@@ -2,7 +2,7 @@ import torch
 from torch.nn import DataParallel, Module
 from torch.nn.parallel import DistributedDataParallel
 from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
-
+import torch.nn as nn
 from model_loader import load_model
 from models import FasterDoFRCNN
 from efficientnet_pytorch import EfficientNet
@@ -15,6 +15,64 @@ class WrappedModel(Module):
     def forward(self, images, targets=None):
         return self.module(images, targets)
 
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+
+
+class C2f(nn.Module):
+    """CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass of a YOLOv5 CSPDarknet backbone layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Applies spatial attention to module's input."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+class SPPF(nn.Module):
+    """Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher."""
+
+    def __init__(self, c1, c2, k=5):  # equivalent to SPP(k=(5, 9, 13))
+        super().__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * 4, c2, 1, 1)
+        self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
+
+    def forward(self, x):
+        """Forward pass through Ghost Convolution block."""
+        x = self.cv1(x)
+        y1 = self.m(x)
+        y2 = self.m(y1)
+        return self.cv2(torch.cat((x, y1, y2, self.m(y2)), 1))
 
 class img2poseModel:
     def __init__(
@@ -49,7 +107,38 @@ class img2poseModel:
             self.device = device
 
         # create network backbone
-        backbone = resnet_fpn_backbone(f"resnet{self.depth}", pretrained=True)
+        # backbone = resnet_fpn_backbone(f"resnet{self.depth}", pretrained=True)
+
+        backbone = nn.Sequential(
+                # [from, repeats, module, args]
+                Conv(-1, 64, 3, 2),  # 0-P1/2
+                Conv(-1, 128, 3, 2),  # 1-P2/4
+                C2f(-1, 128, True, 3),  # 2
+                C2f(-1, 128, True, 3),  # 3
+                C2f(-1, 128, True, 3),  # 4
+                Conv(-1, 256, 3, 2),  # 5-P3/8
+                C2f(-1, 256, True, 3),  # 6
+                C2f(-1, 256, True, 3),  # 7
+                C2f(-1, 256, True, 3),  # 8
+                C2f(-1, 256, True, 3),  # 9
+                C2f(-1, 256, True, 3),  # 10
+                C2f(-1, 256, True, 3),  # 11
+                Conv(-1, 512, 3, 2),  # 12-P4/16
+                C2f(-1, 512, True, 3),  # 13
+                C2f(-1, 512, True, 3),  # 14
+                C2f(-1, 512, True, 3),  # 15
+                C2f(-1, 512, True, 3),  # 16
+                C2f(-1, 512, True, 3),  # 17
+                C2f(-1, 512, True, 3),  # 18
+                Conv(-1, 1280, 3, 2),  # 19-P5/32
+                C2f(-1, 1024, True, 3),  # 20
+                C2f(-1, 1024, True, 3),  # 21
+                C2f(-1, 1024, True, 3),  # 22
+                SPPF(-1, 1024, 5),  # 23
+            )
+
+        backbone.out_channels = 1280
+        
         # # Define the EfficientNet backbone
         # print("EfficientNet")
         # conv_stem = torch.nn.Sequential(EfficientNet.from_pretrained('efficientnet-b0')._conv_stem)
